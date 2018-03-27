947a0abb7062f0835e20c90ac5353cfd33d9e838
icestin <ning231@126.com>
2018-03-27 15:42:50 +0800

    修改同样内容

--- Node/HTTP/index.js
+++ Node/HTTP/index.js
@@ -1,4 +1,8 @@
 // 1  git  branch BranchName
 // 2  git checkout BranchName
 // 3  operate file
-//  git commit -a -m 'some message'
+//  git commit -a -m 'some message new content'
+
+// 添加内容 
+// 删去就内容
+// 不断更新内容
