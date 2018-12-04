0fe6a80d7e73075356a4c596dbbe7baae715c246
icestin <ning231@126.com>
2018-12-04 14:49:56 +0800

    新增改动

--- Node/promise/index.js
+++ Node/promise/index.js
@@ -2,6 +2,10 @@
     console.log('a');
     console.log('ccc');
 })()
-console.log('insert into file before this file');
-console.log('new branch content change at the same line');
-console.log('add new line to the file');
+console.log('insert into file before this file 1');
+console.log('new branch content change at the same line 2');
+console.log('add new line to the file 3');
+console.log('new branch content change at the same line 4');
+console.log('add new line to the file 5');
+console.log('new branch content change at the same line 6' );
+console.log('add new line to the file 7');
