3c0d47f8c4ad2ba1f6da3c3351727eb3865dceb9
icestin <ning231@126.com>
2018-12-08 16:43:28 +0800

    删除掉无用log

--- Node/promise/index.js
+++ Node/promise/index.js
@@ -2,10 +2,3 @@
     console.log('a');
     console.log('ccc');
 })()
-console.log('insert into file before this file 1');
-console.log('new branch content change at the same line 2');
-console.log('add new line to the file 3');
-console.log('new branch content change at the same line 4');
-console.log('add new line to the file 5');
-console.log('new branch content change at the same line 6' );
-console.log('add new line to the file 7');
